8a6206a59c31a21b43d6fd182a8e899c41e5913e
BIOCISHERE <bioc.is.here.8@gmail.com>
2023-03-28 17:43:51 +0000

    proyecto hecho, tal vez necesite mas decoracion

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,58 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
 const Home = () => {
+	const [inputValue, setInputValue] = useState("");
+	const [listTodods, setListTodos] = useState([]);
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
+		<div className="container">
+			<h1 className="text-center">List of my todos</h1>
+			<p className="text-center">
+				Click on the input, add your todo and press enter
 			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
+			<p className="text-center">
+				Then to delete, hover on a task and click on the trashcan icon
 			</p>
+			<div className="border border-secondary rounded p-1">
+				<ul className="p-0 mx-0">
+					<li className="border-bottom p-1">
+						<input
+							type="text"
+							placeholder="What needs to be done?"
+							className="w-100 border-0"
+							onChange={(e) => setInputValue(e.target.value)}
+							value={inputValue}
+							onKeyUp={(e) => {
+								if (e.key === "Enter") {
+									if (inputValue === "") {
+										alert("No tasks, add a task");
+									} else {
+										setListTodos(listTodods.concat(inputValue));
+										setInputValue("");
+									}
+								}
+							}}
+						/>
+					</li>
+					{listTodods.map((todo, index) => (
+						<li key={index} className="showAndHide border-bottom my-1 p-1">
+							{todo}
+							<i
+								className="fas fa-trash-alt float-end py-1 pe-2"
+								onClick={() =>
+									setListTodos(
+										listTodods.filter(
+											(todo, currentIndex) => index != currentIndex
+										)
+									)
+								}
+							></i>
+						</li>
+					))}
+				</ul>
+				<h4 className="text-center">{listTodods.length} tasks left</h4>
+			</div>
 		</div>
 	);
 };
